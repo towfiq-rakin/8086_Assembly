98ea0e3e601f37effa6cc9ac5e57d40b034aa52f
Towfiq Omar Rakin <towfiqomarrakin@gmail.com>
2025-09-29 16:44:06 +0600

    Update series_sum.asm

--- series_sum.asm
+++ series_sum.asm
@@ -7,5 +7,5 @@ SERIES:
     ADD AX, CX
     ADD CX, 2       ; Increment CX by 2
     CMP CX, 15      ; Compare CX with 15 ; CX - 15
-    JBE SERIES      ; CX below or Equal 15 ; CX <= 15
+    JBE SERIES      ; if CX below or Equal 15 ; CX <= 15
 HLT 
